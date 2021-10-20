1195819caba99204eecea2e21657cb77ed8f7655
Aaron Gazzola <aaron@apexapps.com.au>
2021-10-21 08:40:40 +1100

    project init

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link rel='icon' href='/favicon.ico' sizes='any' />
+					<link rel='icon' href='/icon.svg' type='image/svg+xml' />
+					<link rel='apple-touch-icon' href='/apple-touch-icon.png' />
+					<link rel='manifest' href='/manifest.json' />
+				</Head>
+				<body className='bg-background'>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
